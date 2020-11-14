4ec7d3b1e2ad4466c536261bec83a6ed6ea9577e
Zoe <dake0805@gmail.com>
2020-11-14 20:00:31 +0800

    去除 html css 硬编码

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 console.log("background")
+let style = chrome.runtime.getURL("style.css");
+console.log(style)
 chrome.runtime.onMessage.addListener((message, sender) => {
     console.log('timeout')
     switch (message) {
@@ -13,3 +15,17 @@ chrome.runtime.onMessage.addListener((message, sender) => {
         }
     }
 })
+
+
+// chrome.runtime.getPackageDirectoryEntry(function (root) {
+//     root.getFile("style.css", {}, function (fileEntry) {
+//         fileEntry.file(function (file) {
+//             var reader = new FileReader();
+//             reader.onloadend = function (e) {
+//                 this.style = t
+//             };
+//             reader.readAsText(file);
+//         }, errorHandler);
+//     }, errorHandler);
+// });
+
---
+++ utils.js
@@ -0,0 +1,17 @@
+async function getContent(url) {
+    return new Promise((resolve, reject) => {
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', url, true);
+        httpRequest.send();
+
+        console.log(url)
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                resolve(httpRequest.responseText)
+            }
+        };
+        httpRequest.onerror = function (ev) {
+            reject(ev)
+        }
+    })
+}
