91e589a24ee4704eefee13ac164d9a148bf77238
dykai <yingkaidang@gmail.com>
2020-11-14 22:49:08 +0800

    添加动画

--- content.js
+++ content.js
@@ -6,40 +6,40 @@ chrome.runtime.onMessage.addListener(message => {
     }
 })
 
-
+const BREATH_TIME = 5 // seconds
 let breathActive = false
 
 async function active() {
     let breathDiv = document.createElement('breath')
     breathDiv.innerHTML = await getResource("breath.html")
-
     let style = document.createElement('style')
-
     style.innerText = await getResource("style.css")
 
     document.body.prepend(breathDiv)
     document.head.append(style)
     breathActive = true
 
-    const BREATH_TIME = 10 // seconds
     let progress = breathDiv.querySelector('.progress')
     let bar = breathDiv.querySelector('.bar')
-    let closeBtn = breathDiv.querySelector('button')
+    let closeBtn = breathDiv.querySelector('.close button')
     bar.style.cssText = `transition: width ${BREATH_TIME}s linear`
     bar.scrollTop // trigger reflow
     progress.style.cssText = `--percent: 0%`
-    let timerId = setTimeout(() => {
-        breathDiv.remove()
-        style.remove()
-        breath_exit()
-    }, BREATH_TIME * 1000)
+    let timerId = setTimeout(exit, BREATH_TIME * 1000)
 
     closeBtn.addEventListener('click', () => {
         clearInterval(timerId)
-        breathDiv.remove()
-        style.remove()
-        breath_exit()
+        exit()
     })
+
+    function exit() {
+        breathDiv.style.animation = '1s exit ease-out';
+        setTimeout(() => {
+            breathDiv.remove()
+            style.remove()
+            breath_exit()
+        }, 1000)
+    }
 }
 
 function tab_focus() {
